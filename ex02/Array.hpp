f09a702819428ccfcea33f50e3f4df5f6384dbf7
ayoub-essarghini <aes-sarg@student.1337.ma>
2024-12-05 22:54:24 +0100

    implement Array.tpp

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,67 +1,22 @@
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
-
+#pragma once
 #include <stdexcept>
 
 template <typename T>
-class Array {
+class Array
+{
 private:
-    T* _elements;         // Pointer to the array elements
-    unsigned int _size;   // Number of elements in the array
+    T *_elements;
+    unsigned int _size;
 
 public:
-    // Default constructor: Creates an empty array
-    Array() : _elements(NULL), _size(0) {}
-
-    // Parameterized constructor: Creates an array of n elements initialized by default
-    Array(unsigned int n) : _elements(new T[n]()), _size(n) {}
-
-    // Copy constructor
-    Array(const Array& other) : _elements(new T[other._size]), _size(other._size) {
-        for (unsigned int i = 0; i < _size; i++) {
-            _elements[i] = other._elements[i];
-        }
-    }
-
-    // Destructor
-    ~Array() {
-        delete[] _elements;
-    }
-
-    // Assignment operator
-    Array& operator=(const Array& other) {
-        if (this != &other) {
-            T* temp = new T[other._size];    // Allocate temporary memory
-            for (unsigned int i = 0; i < other._size; i++) {
-                temp[i] = other._elements[i];
-            }
-            delete[] _elements;             // Free existing memory
-            _elements = temp;               // Assign the new memory
-            _size = other._size;            // Copy size
-        }
-        return *this;
-    }
-
-    // Subscript operator for accessing elements
-    T& operator[](unsigned int index) {
-        if (index >= _size) {
-            throw std::out_of_range("Index out of bounds");
-        }
-        return _elements[index];
-    }
-
-    // Const version of the subscript operator
-    const T& operator[](unsigned int index) const {
-        if (index >= _size) {
-            throw std::out_of_range("Index out of bounds");
-        }
-        return _elements[index];
-    }
-
-    // size() member function: Returns the number of elements
-    unsigned int size() const {
-        return _size;
-    }
+    Array();
+    Array(unsigned int n);
+    Array(const Array &other);
+    ~Array();
+    Array &operator=(const Array &other);
+    T &operator[](unsigned int index);
+    const T &operator[](unsigned int index) const;
+    unsigned int size() const;
 };
 
-#endif
+#include "Array.tpp"
---
+++ ex02/Array.tpp
@@ -0,0 +1,65 @@
+#include "Array.hpp"
+#include <stdexcept>
+template <typename T>
+Array<T>::Array() : _elements(NULL), _size(0)
+{
+}
+
+template <typename T>
+Array<T>::Array(unsigned int _size) : _size(_size)
+{
+    _elements = new T[_size];
+}
+
+template <typename T>
+Array<T>::Array(const Array &other) : _elements(new T[other._size]), _size(other._size)
+{
+    for (unsigned int i = 0; i < _size; i++)
+    {
+        _elements[i] = other._elements[i];
+    }
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &other)
+{
+    if (this != &other)
+    {
+        T *temp = new T[other._size]; // Allocate temporary memory
+        for (unsigned int i = 0; i < other._size; i++)
+        {
+            temp[i] = other._elements[i];
+        }
+        delete[] _elements;
+        _elements = temp;
+        _size = other._size;
+    }
+    return *this;
+}
+template <typename T>
+unsigned int Array<T>::size() const
+{
+    return _size;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int index)
+{
+    if (index >= _size)
+        throw std::out_of_range("index out of bound");
+    return _elements[index];
+}
+
+template <typename T>
+const T &Array<T>::operator[](unsigned int index) const
+{
+    if (index >= _size)
+        throw std::out_of_range("index out of range");
+    return _elements[index];
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+    delete[] _elements;
+}
